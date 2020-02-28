390e29295a2676beffd29af0642b953e6ebb966c
taymaa <taymaa.2917@gmail.com>
2020-02-28 11:49:07 +0200

    npm lblblblblb

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 const exprss = require('express');
-const user = require('./routes/user');
-const forgetPassword = require('./routes/forgetPassword');
-const verifyAccount = require('./routes/verifyAccount');
-const bodyParser = require('body-parser');
+// const user = require('./routes/user');
+// const forgetPassword = require('./routes/forgetPassword');
+// const verifyAccount = require('./routes/verifyAccount');
+// const bodyParser = require('body-parser');
 const app = exprss();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-const env = require('dotenv').config();
-require("./config/db");
-app.use('/api/user', user);
-app.use('/api/forgetPassword', forgetPassword);
-app.use('/api/verifyAccount', verifyAccount);
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// const env = require('dotenv').config();
+// require("./config/db");
+// app.use('/api/user', user);
+// app.use('/api/forgetPassword', forgetPassword);
+// app.use('/api/verifyAccount', verifyAccount);
 app.get('/', (req, res) => {
     res.send('hi');
 });
