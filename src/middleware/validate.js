3e10dbf733e2f0063ecc37ff4edc8e8ad5baf3a0
taymaa <taymaa.2917@gmail.com>
2020-03-01 19:28:40 +0200

    modifi status

--- src/middleware/validate.js
+++ src/middleware/validate.js
@@ -4,7 +4,7 @@ const validate = async(req, res, next) => {
     try {
         const { name, email, phone, password } = req.body;
         const data = await User.Vatidation(email, phone);
-        data.success ? next() : res.send({ Error: data.Error });
+        data.success ? next() : res.status(400).json({ Error: data.Error });
     } catch (err) {
         res.status(400).json({ Error: err });
     }
